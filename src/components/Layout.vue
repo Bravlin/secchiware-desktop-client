349fae2f19d05f7c465ae6cc186d977e20677ecb
Bravlin <bravlinleonel@gmail.com>
2020-02-05 13:09:19 -0300

    Icons incorporated

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,9 +1,10 @@
 <template>
     <div id="layout" class="d-flex">
         <div id="navbar" class="d-flex flex-column">
-            <div>Placeh</div>
-            <div>Placeh</div>
-            <div class="mt-auto">Placeh</div>
+            <div class="py-1 navbar-button"><b-icon-cloud-fill class="navbar-icon"></b-icon-cloud-fill></div>
+            <div class="py-1 navbar-button"><b-icon-laptop class="navbar-icon"></b-icon-laptop></div>
+            <div class="py-1 navbar-button"><b-icon-documents class="navbar-icon"></b-icon-documents></div>
+            <div class="py-1 navbar-button mt-auto"><b-icon-gear-fill class="navbar-icon"></b-icon-gear-fill></div>
         </div>
         <b-container id="main-content" fluid></b-container>
     </div>
@@ -20,8 +21,20 @@ export default {
   height: 100vh;
 }
 
-#navbar {
-    width: 100px;
+#navbar, .navbar-button {
+    width: 70px;
+}
+
+#navbar .navbar-button:hover {
+    color:gray;
+    cursor: -webkit-grabbing;
+}
+
+#navbar .navbar-icon {
+    width: 50px;
+    height: 50px;
+    margin-left: auto;
+    margin-right: auto;
 }
 
 #main-content {
