f7c5bbcf6c7fc6f8e9d1f6974100471c5e5e88c7
Bravlin <bravlinleonel@gmail.com>
2020-02-06 13:51:18 -0300

    Layout dynamic panel

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="layout" class="d-flex">
         <div id="navbar" class="d-flex flex-column">
-            <div class="py-1 navbar-button">
+            <div class="py-1 navbar-button" @click="changePanel('command-control-panel')">
                 <b-icon-cloud-fill class="navbar-icon"></b-icon-cloud-fill>
             </div>
             <div class="py-1 navbar-button">
@@ -10,37 +10,38 @@
             <div class="py-1 navbar-button">
                 <b-icon-documents class="navbar-icon"></b-icon-documents>
             </div>
-            <div class="py-1 navbar-button mt-auto">
+            <div class="py-1 navbar-button mt-auto" @click="changePanel('connection-form')">
                 <b-icon-gear-fill class="navbar-icon"></b-icon-gear-fill>
             </div>
         </div>
-        <!-- <b-container id="main-content" fluid></b-container> -->
-        <command-control-panel
-            v-if="environments && availablePackages"
-            :c2="c2"
-            :sessionStart="sessionStart"
-            :environments="environments"
-            :availablePackages="availablePackages"
-            class="main-content flex-grow-1">
-        </command-control-panel>
+        <component
+            :is="currentPanel"
+            v-if="panelConditions"
+            v-bind="panelProps"
+            v-on="panelEvents"
+            class="main-content flex-grow-1"
+        ></component>
     </div>
 </template>
 
 <script>
-import CommandControlPanel from './view_panels/CommandControlPanel.vue'
+import CommandControlPanel from './view_panels/CommandControlPanel.vue';
+import ConnectionForm from './ConnectionForm.vue';
 
 export default {
     name: 'layout',
 
     components: {
-        CommandControlPanel
+        CommandControlPanel,
+        ConnectionForm
     },
 
     data() {
         return {
             sessionStart: new Date(),
             environments: null,
-            availablePackages: null
+            availablePackages: null,
+            currentPanel: 'command-control-panel'
         };
     },
 
@@ -48,6 +49,63 @@ export default {
         c2: {
             type: Object,
             required: true
+        }
+    },
+
+    methods: {
+        changePanel(panel) {
+            this.currentPanel = panel;
+        },
+
+        newConnection(c2) {
+            this.c2 = c2;
+            this.sessionStart = new Date(),
+
+            fetch(this.c2.url + '/test_sets')
+            .then(response => response.json())
+            .then(data => {
+                this.availablePackages = data;
+            });
+
+            fetch(this.c2.url + '/environments')
+            .then(response => response.json())
+            .then(data => {
+                this.environments = data;
+            });
+        }
+    },
+
+    computed: {
+        panelConditions() {
+            switch (this.currentPanel) {
+                case 'command-control-panel':
+                    return this.environments && this.availablePackages;
+                default:
+                    return true;
+            }
+        },
+
+        panelProps() {
+            switch (this.currentPanel) {
+                case 'command-control-panel':
+                    return {
+                        c2: this.c2,
+                        sessionStart: this.sessionStart,
+                        environments: this.environments,
+                        availablePackages: this.availablePackages
+                    };
+                default:
+                    return null;
+            }
+        },
+
+        panelEvents() {
+            switch (this.currentPanel) {
+                case 'connection-form':
+                    return {connected: this.newConnection};
+                default:
+                    return null;
+            }
         }
     },
 
